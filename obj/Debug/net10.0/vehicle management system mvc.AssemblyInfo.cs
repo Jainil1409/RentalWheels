d1d8e917ef59ba2d4264b5635dc387682c5e4d76
Jainil1409 <Jainil1vyas1414@gmail.com>
2026-03-16 19:09:05 +0530

    commit

--- obj/Debug/net10.0/vehicle management system mvc.AssemblyInfo.cs
+++ obj/Debug/net10.0/vehicle management system mvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("vehicle management system mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+915f5aa674045b061d85f20ebf812e2c72c4e83a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc774f191178a728b9fbbf3d4858a8bf98a91ba7")]
 [assembly: System.Reflection.AssemblyProductAttribute("vehicle management system mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("vehicle management system mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
